28e8edf4a84e8acd519caad48a18757819e4d7f5
Ohsarsour <obaidullahsarsour@gmail.com>
2023-10-12 22:45:08 -0400

    testing comment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
+#testing
 # Configuration
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
